08571f5c6c4b03b366a3c9973e08fd79ece4b1f4
Nicolai Parlog <nipa@codefx.org>
2018-04-24 07:00:12 +0200

    Solve process API tasks

--- src/main/java/org/codefx/courses/java9/api/processes/Tasks.java
+++ src/main/java/org/codefx/courses/java9/api/processes/Tasks.java
@@ -1,7 +1,10 @@
 package org.codefx.courses.java9.api.processes;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Container for a set of tasks as collected at one point in time. Immutable.
@@ -15,7 +18,11 @@ public class Tasks {
 	}
 
 	public static Tasks all() {
-		return new Tasks(List.of());
+		List<Task> tasks = ProcessHandle
+				.allProcesses()
+				.map(Task::fromProcessHandle)
+				.collect(toList());
+		return new Tasks(tasks);
 	}
 
 	public static Tasks of(List<Task> tasks) {
