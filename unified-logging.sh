03d6ec39419d0954c0d57b214d8db8842fc4ff69
Nicolai Parlog <nipa@codefx.org>
2018-10-05 19:25:17 +0200

    Solve unified logging tasks

--- unified-logging.sh
+++ unified-logging.sh
@@ -6,22 +6,22 @@ echo "OBSERVE: RUN `java -Xlog:help` AND FAMILIARIZE YOURSELF WITH THE HELP"
 
 echo "----- ----- ----- ----- -----"
 echo "TASK: SHOW ALL info MESSAGES TAGGED ONLY WITH gc"
-$JAVA -version
+$JAVA -Xlog:gc -version
 
 echo "----- ----- ----- ----- -----"
 echo "TASK: SHOW ALL info MESSAGES TAGGED WITH gc"
-$JAVA -version
+$JAVA -Xlog:gc* -version
 
 echo "----- ----- ----- ----- -----"
 echo "TASK: SHOW ALL MESSAGES TAGGED WITH gc"
-$JAVA -version
+$JAVA -Xlog:gc*:trace -version
 
 # TASK: Make sense of these MESSAGES (nah, just kidding ;)
 
 echo "----- ----- ----- ----- -----"
 echo "TASK: DIVERT ALL gc-MESSAGES INTO A FILE `gc.log`"
-$JAVA -version
+$JAVA -Xlog:gc*=trace:file=gc.log -version
 
 echo "----- ----- ----- ----- -----"
 echo "TASK: SHOW date&time, level, AND thread id OF ALL gc-MESSAGES"
-$JAVA -version
+$JAVA -Xlog:gc*=trace:stdout:time,level,tid -version
