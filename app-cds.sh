12d8efe470c39f160eef5ce2da9c99af6ed86a87
Nicolai Parlog <nipa@codefx.org>
2018-10-05 21:17:26 +0200

    Solve App-CDS

--- app-cds.sh
+++ app-cds.sh
@@ -5,10 +5,24 @@ source executables.sh
 echo "TASK: CREATE CLASS LIST"
 # HINT: the last command in this file launches this app
 #       (run `mvn clean install` beforehand)
+$JAVA \
+	-XX:DumpLoadedClassList=app-cds.lst \
+	--class-path target/java-x-1.0-SNAPSHOT.jar \
+	org.codefx.courses.java9.api.stream.Iterate
 
 echo "TASK: CREATE ARCHIVE"
+$JAVA \
+	-Xshare:dump \
+	-XX:SharedClassListFile=app-cds.lst \
+	-XX:SharedArchiveFile=app-cds.jsa \
+	--class-path target/java-x-1.0-SNAPSHOT.jar
 
 echo "TASK: LAUNCH APPLICATION WITH ARCHIVE AND MEASURE RUN TIME"
+time $JAVA \
+	-Xshare:on \
+	-XX:SharedArchiveFile=app-cds.jsa \
+	--class-path target/java-x-1.0-SNAPSHOT.jar \
+	org.codefx.courses.java9.api.stream.Iterate
 
 echo "OBSERVE: LAUNCH TIME IMPROVEMENT"
 time $JAVA \
